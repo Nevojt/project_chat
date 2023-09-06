f1dbfd8ccb78f5cbcbf3652f54387c61653d484a
Nevojt <nevojt@gmail.com>
2023-09-06 12:21:05 +0200

    created room_four, room_five to database created models rooms append function main.py

--- app/models.py
+++ app/models.py
@@ -44,9 +44,38 @@ class RoomThree(Base):
     is_privat = Column(Boolean, server_default='False', nullable=False)
     receiver = Column(Integer, nullable=False)
     
+class RoomFour(Base):
+    __tablename__ = 'room_four'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    name = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    member_id = Column(Integer,  nullable=False)
+    avatar = Column(String, nullable=False)
+    is_privat = Column(Boolean, server_default='False', nullable=False)
+    receiver = Column(Integer, nullable=False)
+    
+class RoomFive(Base):
+    __tablename__ = 'room_five'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    name = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    member_id = Column(Integer,  nullable=False)
+    avatar = Column(String, nullable=False)
+    is_privat = Column(Boolean, server_default='False', nullable=False)
+    receiver = Column(Integer, nullable=False)
+    
+    
+    
+    
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, nullable=False)
     user_name = Column(String, nullable=False)   
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    token = Column(Integer, nullable=False)
+    token = Column(String, nullable=False)
