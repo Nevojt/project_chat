f242fa56eca1a18baaa9ad238e14684ea024ed16
Nevojt <nevojt@gmail.com>
2023-09-12 16:17:59 +0200

    delete file room_* delete classes room* create file utils hash

--- app/models.py
+++ app/models.py
@@ -4,42 +4,7 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 
-class BaseRoom(Base):
-    __abstract__ = True
-    
-    id = Column(Integer, primary_key=True, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    name = Column(String, nullable=False)
-    message = Column(String, nullable=False)
-    published = Column(Boolean, server_default='TRUE', nullable=False)
-    member_id = Column(Integer,  nullable=False)
-    avatar = Column(String, nullable=False)
-    is_privat = Column(Boolean, server_default='False', nullable=False)
-    receiver = Column(Integer, nullable=False)
 
-
-
-
-class RoomOne(BaseRoom):
-    __tablename__ = 'room_one'
-    
-    
-class RoomTwo(BaseRoom):
-    __tablename__ = 'room_two'
-    
-    
-class RoomThree(BaseRoom):
-    __tablename__ = 'room_three'
-    
-    
-    
-class RoomFour(BaseRoom):
-    __tablename__ = 'room_four'
-    
-    
-    
-class RoomFive(BaseRoom):
-    __tablename__ = 'room_five'
     
 class Rooms(Base):
     __tablename__ = 'rooms'
