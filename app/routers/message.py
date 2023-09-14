7df1a4813eb76a6799ce815bc990f2925b708d09
Nevojt <nevojt@gmail.com>
2023-09-14 19:05:45 +0200

    Update authorization of the current users

--- app/routers/message.py
+++ app/routers/message.py
@@ -10,16 +10,16 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def get_posts(db: Session = Depends(get_db), cuser_id: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Message).all()
     return posts
 
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.MessagePost)
-async def create_posts(post: schemas.MessageCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def create_posts(post: schemas.MessageCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
-    print(user_id)
+    print(current_user.user_name)
     post = models.Message(**post.dict())
     db.add(post)
     db.commit()
@@ -29,7 +29,7 @@ async def create_posts(post: schemas.MessageCreate, db: Session = Depends(get_db
 
 
 @router.get("/{rooms}", response_model=schemas.MessagePost)
-async def get_post(rooms: str, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+async def get_post(rooms: str, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
     post = db.query(models.Message).filter(models.Message.rooms == rooms).first()
     
@@ -40,7 +40,7 @@ async def get_post(rooms: str, db: Session = Depends(get_db), user_id: int = Dep
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Message).filter(models.Message.id == id)
     
     if post.first() == None:
@@ -52,15 +52,15 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.put("/{rooms}", response_model=schemas.MessagePost)
-def update_post(rooms: str, update_post: schemas.MessageCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+@router.put("/{id}", response_model=schemas.MessagePost)
+def update_post(id: int, update_post: schemas.MessageCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     
-    post_query = db.query(models.Message).filter(models.Message.rooms == rooms)
+    post_query = db.query(models.Message).filter(models.Message.id == id)
     post = post_query.first()
     
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with rooms: {rooms} not found")
+                            detail=f"post with id: {id} not found")
     
     post_query.update(update_post.dict(), synchronize_session=False)
     
