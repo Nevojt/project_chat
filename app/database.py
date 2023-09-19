ffe92bb9b5f891d2cbef937c1f377d701643a9de
Nevojt <nevojt@gmail.com>
2023-09-19 15:01:14 +0200

    Clean code created environment variables for database or created tokens

--- app/database.py
+++ app/database.py
@@ -1,15 +1,14 @@
-from dotenv import load_dotenv
-load_dotenv()
 
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from .config import settings
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://postgres:{key}@{url}:5432/postgres'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_name}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_username}'
 
 
 
@@ -25,3 +24,16 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+while True:   
+    try:
+        conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name, user=settings.database_username,
+                                password=settings.database_password, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+
+    except Exception as error:
+            print('Conection to database failed')
+            print("Error:",  error)
+            time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,21 +1,9 @@
-import time
-from dotenv import load_dotenv
-
-load_dotenv()
-import os
 
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
-
 from . import models
 from .database import engine
 from .routers import message, user, rooms, auth, user_status
-
-
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
+# from .config import settings
 
 
 
@@ -24,33 +12,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-    
-while True:   
-    try:
-        conn = psycopg2.connect(host=url, database='postgres', user='postgres',
-                                password=key, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful")
-        break
-
-    except Exception as error:
-            print('Conection to database failed')
-            print("Error:",  error)
-            time.sleep(2)
-    
-my_post = [{"name": "big boss", "message": "content of post 1", "id": 1}, 
-           {"name": "litle foot", "message":"favorite Pizza", "id": 2 }]
-
-
-def find_post(id):
-    for p in my_post:
-        if p["id"] == id:
-            return p
-        
-def find_index_post(id):
-    for i, p in enumerate(my_post):
-        if p["id"] == id:
-            return i
 
 app.include_router(message.router)
 app.include_router(rooms.router)
