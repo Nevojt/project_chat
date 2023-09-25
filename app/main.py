7ed5ab10cb688133ef363218386e1643b114d321
Nevojt <nevojt@gmail.com>
2023-09-25 21:11:19 +0200

    Created CORS headers

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from starlette.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import message, user, rooms, auth, user_status, vote
@@ -11,6 +13,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.include_router(message.router)
@@ -20,3 +32,7 @@ app.include_router(user_status.router)
 app.include_router(vote.router)
 
 app.include_router(user.router)
+
+@app.get('/')
+def start():
+    return {"status": "Hello World"}
