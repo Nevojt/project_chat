cfd5b6f193e73dd8928a7259b433efcd97071b95
Nevojt <nevojt@gmail.com>
2023-10-03 21:44:23 +0200

    crated first socket

--- soket.py
+++ soket.py
@@ -2,39 +2,29 @@
 from fastapi import status, HTTPException, Depends, APIRouter, Response, WebSocket
 from sqlalchemy.orm import Session
 from app.database import get_db
-router = APIRouter()
+from app import models, schemas
 
-messages = ["hello", "world"]
+router = APIRouter(
+    prefix="/ws",
+    tags="Socket"
+)
 
 
-@router.websocket("/ws")
+
+@router.websocket("/")
 async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)):
     await websocket.accept()
-
-    # Відправте всі збережені повідомлення новому користувачеві
-    for message in db:
-        await websocket.send_text(message)
-
-    # Очікуйте нові повідомлення
     while True:
         data = await websocket.receive_text()
-        messages.append(data)  # Збережіть нове повідомлення
-        await websocket.send_text(f"Повідомлення отримано: {data}")
+        message_data = schemas.MessagePost.parse_raw(data)
+        message = save_message(db, message_data)
+        await websocket.send_text(f"Message saved with ID: {message.id}")
 
-active_websockets = []
 
-@router.websocket("/ws/{room}")
-async def websocket_endpoint(websocket: WebSocket, room: str):
-    await websocket.accept()
-    active_websockets.append((room, websocket))
-    
-    try:
-        while True:
-            data = await websocket.receive_text()
-            # Відправте повідомлення всім підключеним користувачам в цій кімнаті
-            for r, ws in active_websockets:
-                if r == room:
-                    await ws.send_text(data)
-    except:
-        # Коли користувач відключається, видаліть його веб-сокет зі списку
-        active_websockets.remove((room, websocket))
+
+def save_message(db: Session, message_data: schemas.MessagePost):
+    message = models.Message(**message_data.dict())
+    db.add(message)
+    db.commit()
+    db.refresh(message)
+    return message
