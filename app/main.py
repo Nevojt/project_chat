28e53b7e8c157704a264f3afc60a7b93ef898aaf
Nevojt <nevojt@gmail.com>
2023-10-14 14:25:18 +0200

    created static and templates home page api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
-from . import models
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from .database import engine
 from .routers import message, user, rooms, auth, user_status, vote, images, token_socket
 from app import models
@@ -32,36 +32,11 @@ app.include_router(token_socket.router)
 
 app.include_router(user.router)
 
-@app.get("/", response_class=HTMLResponse)
-def home():
-    html_content = """<!DOCTYPE html>
-            <html lang="uk">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Привітання</title>
-                <style>
-                    body {
-                        font-family: Arial, sans-serif;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        height: 100vh;
-                        background-color: #f4f4f4;
-                    }
-                    .greeting {
-                        padding: 20px;
-                        background-color: #fff;
-                        border-radius: 8px;
-                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="greeting">
-                    <h1>Ласкаво просимо!</h1>
-                    <p>Це сторінка привітання. Насолоджуйтеся!</p>
-                </div>
-            </body>
-            </html>"""
-    return html_content
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
