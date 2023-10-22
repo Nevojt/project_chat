ecd3866939b79950eef3ca02194bcf1111db6e73
Nevojt <nevojt@gmail.com>
2023-10-22 17:50:45 +0200

    test token user

--- tests/test_users.py
+++ tests/test_users.py
@@ -29,12 +29,11 @@ def test_created_user():
     
 def test_login_user(test_user, client):
     res = client.post(
-        "login", data={"username": test_user['email'], "password": test_user['password']}
-    )
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
     login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]) #
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
     id = payload.get("user_id")
-    
-    assert id ==test_user['id']
+    assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
