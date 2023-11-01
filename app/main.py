1965eabce8e0ac8db64c92b040746b9b8e00cac0
Nevojt <nevojt@gmail.com>
2023-11-01 10:52:10 +0100

    rename socket_new -> socket

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from .database import engine
-from .routers import message, socket, user, rooms, auth, user_status, vote, images, socket_new
+from .routers import message, socket, user, rooms, auth, user_status, vote, images
 from app import models
 
 models.Base.metadata.create_all(bind=engine)
@@ -28,9 +28,7 @@ app.include_router(auth.router)
 app.include_router(user_status.router)
 app.include_router(vote.router)
 app.include_router(images.router)
-# app.include_router(socket.router)
-app.include_router(socket_new.router)
-
+app.include_router(socket.router)
 app.include_router(user.router)
 
 
