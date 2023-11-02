c1d708d3329bb715b6ade7a2c556c6b5b52e5d77
Nevojt <nevojt@gmail.com>
2023-11-02 10:53:58 +0100

    sorting private messages to recipients and sender

--- app/routers/private_messages.py
+++ app/routers/private_messages.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+import json
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
 from app.connection_manager import ConnectionManager
 from app.database import get_async_session
 from app import models, schemas, oauth2
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import desc
+from sqlalchemy import and_, desc, or_
 from typing import List
 
 router = APIRouter()
@@ -19,30 +20,32 @@ manager = ConnectionManager()
 
 
 
-async def fetch_last_private_messages(session: AsyncSession, recipient_id: int) -> List[schemas.PrivateMessage]:
+async def fetch_last_private_messages(session: AsyncSession, sender_id: int, recipient_id: int) -> List[dict]:
     query = select(models.PrivateMessage, models.User).join(
         models.User, models.PrivateMessage.sender_id == models.User.id
     ).where(
-        models.PrivateMessage.recipient_id == recipient_id
+        or_(
+            and_(models.PrivateMessage.sender_id == sender_id, models.PrivateMessage.recipient_id == recipient_id),
+            and_(models.PrivateMessage.sender_id == recipient_id, models.PrivateMessage.recipient_id == sender_id)
+        )
     ).order_by(desc(models.PrivateMessage.id)).limit(5)
 
     result = await session.execute(query)
     raw_messages = result.all()
 
     messages = [
-        schemas.PrivateMessage(
-            created_at=private.created_at,
-            recipient_id=private.recipient_id,
-            sender_id=private.sender_id,
-            messages=private.messages,
-            user_name=user.user_name,
-            avatar=user.avatar
-                # Ensure messages attribute is passed
-        )
+        {
+            "created_at": private.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+            "sender_id": private.sender_id,
+            "message": private.messages,
+            "user_name": user.user_name,
+            "avatar": user.avatar,
+        }
         for private, user in raw_messages
     ]
 
     return messages
+
 
 
 
@@ -56,11 +59,13 @@ async def private_webprivate_endpoint(
     user = await oauth2.get_current_user(token, session)
 
     await manager.connect(websocket, user.id, user.user_name, user.avatar)
-    messages = await fetch_last_private_messages(session, recipient_id)
+    messages = await fetch_last_private_messages(session, user.id, recipient_id)
+
 
     for message in messages:  
-        await websocket.send_text(message.model_dump_json())  # Use json() instead of model_dump_json()
-        print(message.model_dump_json())
+        message_json = json.dumps(message, ensure_ascii=False)
+        await websocket.send_text(message_json)
+        print(message_json)
 
     try:
         while True:
