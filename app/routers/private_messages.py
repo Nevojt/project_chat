4a8d790c849f8d81a30f38448fea8b60b9aaf499
Nevojt <nevojt@gmail.com>
2023-11-15 20:45:42 +0100

    change of route

--- app/routers/private_messages.py
+++ app/routers/private_messages.py
@@ -5,8 +5,8 @@ from ..database import get_db
 from .. import models, schemas, oauth2
 
 router = APIRouter(
-    prefix="/private",
-    tags=['Private'],
+    prefix="/direct",
+    tags=['Direct'],
 )
 
 
@@ -43,23 +43,3 @@ async def get_private_messages(recipient_id: int, db: Session = Depends(get_db))
         ]
         
     return results
-
-
-
-
-
-
-
-# @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.RoomPost)
-# async def create_room(room: schemas.RoomCreate, db: Session = Depends(get_db), current_user: str = Depends(oauth2.get_current_user)):
-    
-#     existing_room = db.query(models.Rooms).filter(models.Rooms.name_room == room.name_room).first()
-#     if existing_room:
-#         raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY,
-#                             detail=f"Room {existing_room.name_room} already exists")
-    
-#     room = models.Rooms(**room.model_dump())
-#     db.add(room)
-#     db.commit()
-#     db.refresh(room)    
-#     return room
