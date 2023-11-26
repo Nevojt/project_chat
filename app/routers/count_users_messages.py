4fdd7bcdbf270ad96b228471b4eae27b0169222b
Nevojt <nevojt@gmail.com>
2023-11-26 10:38:32 +0100

    created count users from rooms

--- app/routers/count_users_messages.py
+++ app/routers/count_users_messages.py
@@ -13,9 +13,25 @@ router = APIRouter(
 
 
 @router.get("/messages", response_model=List[schemas.CountMessages])
-async def get_posts(db: Session = Depends(get_db)):
-    counts = db.query(models.Socket.rooms, func.count(models.Socket.id).label('count')).group_by(models.Socket.rooms).all()
-    if not counts:
+async def get_count_messages(db: Session = Depends(get_db)):
+    query_result = db.query(models.Socket.rooms, func.count(models.Socket.id).label('count')).group_by(models.Socket.rooms).all()
+    
+    if not query_result:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="No messages found in any room.")
+    counts = [{"rooms": rooms, "count": count} for rooms, count in query_result]
+
+    return counts
+
+
+
+@router.get("/users", response_model=List[schemas.CountUsers])
+async def get_count_users(db: Session = Depends(get_db)):
+    query_result = db.query(models.User_Status.name_room, func.count(models.User_Status.id).label('count')).group_by(models.User_Status.name_room).all()
+    
+    if not query_result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No users found in any room.")
+    counts = [{"rooms": name_room, "count": count} for name_room, count in query_result]
+    
     return counts
