115dfaed2f268827cd3acac5596ff6620a018140
Nevojt <nevojt@gmail.com>
2023-12-18 23:28:56 +0100

    database not asynchronously

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,6 @@
 from fastapi import status, HTTPException, Depends, APIRouter
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from ..database import get_async_session
 from ..database import get_db
 from .. import models, schemas, utils, oauth2, send_mail
-from typing import List
 
 router = APIRouter(
     prefix="/users",
@@ -14,7 +9,7 @@ router = APIRouter(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-async def created_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_async_session)):
+async def created_user(user: schemas.UserCreate, db: Depends = Depends(get_db)):
     """
     Creates a new user in the database with the provided user details. It also checks for email uniqueness and hashes the password.
 
@@ -31,15 +26,11 @@ async def created_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_
     
     
     # Check if a user with the given email already exists
-    stmt = select(models.User).where(models.User.email == user.email)
-
-    # Execute the statement asynchronously
-    result = await db.execute(stmt)
-    existing_user = result.scalar_one_or_none()
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
 
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail=f"User with email {user.email} already exists")
+        raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY,
+                            detail=f"User {existing_user.email} already exists")
 
     
     # Hash the user's password
@@ -49,14 +40,14 @@ async def created_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_
     # Create a new user and add it to the database
     new_user = models.User(**user.model_dump())
     db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user) 
+    db.commit()
+    db.refresh(new_user) 
     
     # Create a User_Status entry for the new user
     post = models.User_Status(user_id=new_user.id, user_name=new_user.user_name, name_room="Hell")
     db.add(post)
-    await db.commit()
-    await db.refresh(post)
+    db.commit()
+    db.refresh(post)
     
     
     
