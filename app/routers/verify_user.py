a162d375d830e54d39baa592adfd9d13e698184e
Nevojt <nevojt@gmail.com>
2023-12-21 19:58:39 +0100

    commit test verification

---
+++ app/routers/verify_user.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.templating import Jinja2Templates
+from app import oauth2
+from ..database import get_async_session
+
+
+router = APIRouter(
+    tags=["Verify"]
+)
+
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/verify_email")
+async def verify_email(token: str, request: Request, db: AsyncSession = Depends(get_async_session)):
+    """
+    Verifies the user's email address using the provided token.
+
+    Args:
+        token (str): The token received from the user's email.
+        db (AsyncSession): Asynchronous database session.
+
+    Raises:
+        HTTPException: If the token is invalid or the user is not found.
+
+    Returns:
+        dict: A message confirming email verification.
+    """
+    user = await oauth2.get_current_user(token, db)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid or expired token")
+
+    user.verified = True
+    db.add(user)
+    await db.commit()
+
+    return templates.TemplateResponse("success_registration.html", {"request": request})
