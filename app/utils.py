405d93013fe00becf3b5ac711632302375fcb699
Nevojt <nevojt@gmail.com>
2023-12-24 21:31:51 +0100

    created func generate_unique_token

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,6 @@
 from passlib.context import CryptContext
+import secrets
+import hashlib
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -8,3 +10,12 @@ def hash(password: str):
 
 def verify(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def generate_unique_token(email: str) -> str:
+    # Create a base token using a new random number
+    base_token = secrets.token_urlsafe()
+    # Use the user's email to personalize the token
+    personalized_token = f"{base_token}{email}"
+    # Create a SHA256 hash of the personalized token
+    return hashlib.sha256(personalized_token.encode()).hexdigest()
