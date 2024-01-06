370dbaa28eaf2b0b0759e4fda512a17d61ed7477
Nevojt <nevojt@gmail.com>
2024-01-06 18:30:57 +0100

    created logger

--- app/routers/private_messages.py
+++ app/routers/private_messages.py
@@ -13,50 +13,47 @@ logging.basicConfig(filename='log/private.log', format='%(asctime)s - %(levelnam
 logger = logging.getLogger(__name__)
 
 
-# @router.get("/")
-# async def get_all_private_messages(db: Session = Depends(get_db)):
-#     query = db.query(models.PrivateMessage).all()
-#     return query
-
-
 @router.get("/{user_id}", response_model=List[schemas.PrivateInfoRecipient])
 async def get_private_recipient(user_id: int, db: Session = Depends(get_db)):
-    # Query for recipients and senders
-    messages_query = db.query(models.PrivateMessage, models.User).join(
-        models.User, models.PrivateMessage.recipient_id == models.User.id
-    ).filter(
-        (models.PrivateMessage.sender_id == user_id) | (models.PrivateMessage.recipient_id == user_id)
-    )
+    try:
+        # Query for recipients and senders
+        messages_query = db.query(models.PrivateMessage, models.User).join(
+            models.User, models.PrivateMessage.recipient_id == models.User.id
+        ).filter(
+            (models.PrivateMessage.sender_id == user_id) | (models.PrivateMessage.recipient_id == user_id)
+        )
 
-    # Execute query
-    messages = messages_query.all()
+        # Execute query
+        messages = messages_query.all()
 
-    # Filter and map results
-    read_users = {}
-    unread_users = {}
-    for message, user in messages:
-        if message.recipient_id == user_id and not message.is_read:
-            continue  # Skip unread messages where the user is the recipient
+        # Filter and map results
+        read_users = {}
+        unread_users = {}
+        for message, user in messages:
+            if message.recipient_id == user_id and not message.is_read:
+                continue  # Skip unread messages where the user is the recipient
 
-        other_user_id = message.sender_id if message.recipient_id == user_id else message.recipient_id
-        other_user = db.query(models.User).filter(models.User.id == other_user_id).first()
+            other_user_id = message.sender_id if message.recipient_id == user_id else message.recipient_id
+            other_user = db.query(models.User).filter(models.User.id == other_user_id).first()
 
-        target_dict = read_users if message.is_read and message.recipient_id == user_id else unread_users
-        if other_user_id not in target_dict:
-            target_dict[other_user_id] = schemas.PrivateInfoRecipient(
-                recipient_id=other_user_id,
-                recipient_name=other_user.user_name,
-                recipient_avatar=other_user.avatar,
-                verified=other_user.verified,
-                is_read=message.is_read if message.recipient_id == user_id else False
-            )
+            target_dict = read_users if message.is_read and message.recipient_id == user_id else unread_users
+            if other_user_id not in target_dict:
+                target_dict[other_user_id] = schemas.PrivateInfoRecipient(
+                    recipient_id=other_user_id,
+                    recipient_name=other_user.user_name,
+                    recipient_avatar=other_user.avatar,
+                    verified=other_user.verified,
+                    is_read=message.is_read if message.recipient_id == user_id else False
+                )
 
-    # Combine and sort results
-    result = list(read_users.values()) + list(unread_users.values())
+        # Combine and sort results
+        result = list(read_users.values()) + list(unread_users.values())
 
-    if not result:  
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Sorry, no recipients or senders found.")
+        if not result:  
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Sorry, no recipients or senders found.")
+    except Exception as e:
+        logger.error(f"Error creating {e}", exc_info=True)
     return result
 
 
