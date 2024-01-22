02e0fe96bfec4e498307eed5e5a83c27a214a745
Nevojt <nevojt@gmail.com>
2024-01-22 19:04:21 +0100

    commit back

--- static/script.js
+++ static/script.js
@@ -1,35 +1,28 @@
 document.getElementById('submitBtn').addEventListener('click', async function() {
     var password1 = document.getElementById('password1').value;
     var password2 = document.getElementById('password2').value;
-
     if (!isValidPassword(password1)) {
-        document.getElementById('message').textContent = 'The password must be between 4 and 8 characters long, contain at least one uppercase letter, one number, and one special character @#$%^&+=!?.';
+        document.getElementById('message').textContent = 'The password must be between 4 and 8 characters long, contain at least one uppercase letter, one number, and one special character.';
         return;
     }
-
     if (password1 !== password2) {
         document.getElementById('message').textContent = 'Passwords do not match!';
         return;
     }
-
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token'); // Отримання токену з URL
-
     try {
-        const response = await fetch(`https://cool-chat.club/reset?token=${token}`, {
+        const response = await fetch(`/password/reset?token=${token}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ password: password1 })
         });
-
-
         const data = await response.json();
-
         if (response.ok) {
             // Успішне скидання пароля
-            window.location.href = '/success-page';
+            window.location.href = '/success-page'; // Перенаправлення на сторінку успіху
         } else {
             console.error('Error resetting password:', data);
             document.getElementById('message').textContent = 'Error resetting password';
@@ -39,25 +32,7 @@ document.getElementById('submitBtn').addEventListener('click', async function() 
         document.getElementById('message').textContent = 'Error resetting password';
     }
 });
-
 function isValidPassword(password) {
     var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!?]).{4,8}$/;
     return passwordRegex.test(password);
 }
-
-function togglePassword(inputId) {
-    var passwordInput = document.getElementById(inputId);
-    var toggleIcon = document.querySelector('.toggle-' + inputId);
-    var toggleIconSlash = document.querySelector('.toggle-' + inputId + '-slash');
-
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        toggleIcon.style.display = 'none';
-        toggleIconSlash.style.display = 'block';
-    } else {
-        passwordInput.type = 'password';
-        toggleIcon.style.display = 'block';
-        toggleIconSlash.style.display = 'none';
-    }
-}
-
