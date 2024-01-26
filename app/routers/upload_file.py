0605030f3707a71842a58a171da2d6fd9f3e3c10
Nevojt <nevojt@gmail.com>
2024-01-26 21:56:48 +0100

    created download files to supabase

--- app/routers/upload_file.py
+++ app/routers/upload_file.py
@@ -1,26 +1,41 @@
 from typing import Annotated
-
 from fastapi import File, UploadFile, APIRouter
-
-import os
 from supabase import create_client, Client
+from app.config_supabase import settings
+from typing import Optional
+import os
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = settings.supabase_url
+key: str = settings.supabase_key
 supabase: Client = create_client(url, key)
 
 router = APIRouter(
     prefix="/upload",
-    tags=['Upload file'],
-    
+    tags=['Upload file'],  
 )
 
 
-@router.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+def public_url(bucket_name, file_path):
+    res = supabase.storage.from_(bucket_name).get_public_url(file_path)
+    return res
 
 
-@router.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.size}
+
+@router.post("/upload-to-supabase/")
+async def upload_to_supabase(file: UploadFile = File(...), bucket_name: str = "image_chat"):
+    try:
+        file_path = file.filename
+        file_mime_type = file.content_type
+        contents = await file.read()
+
+        upload_response = supabase.storage.from_(bucket_name).upload(file_path, contents, file_options={"contentType": file_mime_type})
+        
+        return public_url(bucket_name, file_path)
+
+    except Exception as error:
+        return {"error": str(error)}
+
+
+
+
+
