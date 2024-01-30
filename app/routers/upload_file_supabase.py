b4fbff08203e416e2fd3117db8eb540053d0c2e6
Nevojt <nevojt@gmail.com>
2024-01-30 20:08:44 +0100

    Created docstring from supabase

--- app/routers/upload_file_supabase.py
+++ app/routers/upload_file_supabase.py
@@ -1,9 +1,8 @@
-from typing import Annotated
+
 from fastapi import File, UploadFile, APIRouter
 from supabase import create_client, Client
 from app.config_supabase import settings
-from typing import Optional
-import os
+
 
 url: str = settings.supabase_url
 key: str = settings.supabase_key
@@ -19,10 +18,21 @@ def public_url(bucket_name, file_path):
     res = supabase.storage.from_(bucket_name).get_public_url(file_path)
     return res
 
-
-
 @router.post("/upload-to-supabase/")
 async def upload_to_supabase(file: UploadFile = File(...), bucket_name: str = "image_chat"):
+    """
+    This function is used to upload a file to supabase storage.
+
+    Args:
+        file (UploadFile): The file to be uploaded.
+        bucket_name (str): The name of the bucket where the file should be uploaded.
+
+    Returns:
+        str: The public URL of the uploaded file.
+
+    Raises:
+        Exception: If there is an error during the upload process.
+    """
     try:
         file_path = file.filename
         file_mime_type = file.content_type
