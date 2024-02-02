8afbed9c46cb5a918403e5078dc341cff653ec62
Nevojt <nevojt@gmail.com>
2024-02-02 20:04:26 +0100

    created test upload test_download_google_storage

---
+++ tests/test_download_google_storage.py
@@ -0,0 +1,18 @@
+import io
+from fastapi.testclient import TestClient
+from app.routers.upload_file_google import create_upload_file
+
+
+client = TestClient(create_upload_file)
+
+def test_create_upload_file():
+    # Arrange
+    test_file = io.BytesIO(b"test content")
+    test_file_name = "test_file.txt"
+
+    # Act
+    response = client.post("/uploadfile/", files={"file": test_file, "filename": test_file_name})
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == {"filename": test_file_name, "public_url": f"https://storage.googleapis.com/chatbuscket/test_file.txt"}
