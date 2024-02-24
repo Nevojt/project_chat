adf9f25101eea531e7c8ec06fc1d9d3222eaecd3
Nevojt <nevojt@gmail.com>
2024-02-24 11:14:28 +0100

    root path change

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from app import models, send_mail
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
+    root_path="/api"
     title="Cool Chat",
     description="Cool Chat",
     docs_url="/docs",
