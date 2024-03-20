258380c08ab055d12ef3007c6aaaf6c435a24876
Nevojt <nevojt@gmail.com>
2024-03-20 20:11:59 +0100

    append search room and users return two lists

--- app/routers/user/finds.py
+++ app/routers/user/finds.py
@@ -6,7 +6,7 @@ from sqlalchemy import func
 from typing import List
 from ...database.database import get_db
 from app.models import models
-from app.schemas import user
+from app.schemas import user as user_schema
 from app.schemas import room as schema_room
 
 
@@ -15,52 +15,29 @@ router = APIRouter(
     tags=['Search'],
 )
 
-@router.get('/user/{substring}', response_model=List[user.UserInfo])
-def get_user_name(substring: str, db: Session = Depends(get_db)):
+@router.get('/{substring}')
+def search_users_and_rooms(substring: str, db: Session = Depends(get_db)):
     """
-    Get a list of users filtered by a substring.
+    Search for users and rooms based on a substring.
 
     Parameters:
     - `substring`: The substring to filter by.
 
     Returns:
-    - A list of users that match the given substring.
-
-    Raises:
-    - HTTPException 404 if no users match the given substring.
+    - A dictionary containing two lists: one for users and one for rooms.
     """
+    pattern = f"%{substring.lower()}%"
     
-    pattern = f"{substring.lower()}%"  # Matches any username starting with 'substring'
-    # Query the database for a user with the given username
-    user = db.query(models.User).filter(func.lower(models.User.user_name).like(pattern)).all()
-    # If the user is not found, raise an HTTP 404 error
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User {pattern} not found")
-        
-    return user
-
-@router.get('/room/{substring}', response_model=List[schema_room.RoomBase])
-def get_rooms(substring: str, db: Session = Depends(get_db)):
-    """
-    Get a list of rooms filtered by a substring.
-
-    Parameters:
-    - `substring`: The substring to filter by.
-
-    Returns:
-    - A list of rooms that match the given substring.
-
-    Raises:
-    - HTTPException 404 if no rooms match the given substring.
-    """
+    # Search for users
+    users = db.query(models.User).filter(func.lower(models.User.user_name).like(pattern)).all()
     
-    pattern = f"{substring.lower()}%"
-    rooms = db.query(models.Rooms).filter(models.Rooms.name_room != 'Hell', models.Rooms.secret_room != True, func.lower(models.Rooms.name_room).like(pattern)).all()
+    # Search for rooms
+    rooms = db.query(models.Rooms).filter(
+        models.Rooms.name_room != 'Hell', 
+        models.Rooms.secret_room != True, 
+        func.lower(models.Rooms.name_room).like(pattern)
+    ).all()
 
-    if not rooms:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Room {pattern} not found")
     # Count messages for room
     messages_count = db.query(
         models.Socket.rooms, 
@@ -72,8 +49,19 @@ def get_rooms(substring: str, db: Session = Depends(get_db)):
         models.User_Status.name_room, 
         func.count(models.User_Status.id).label('count')
     ).group_by(models.User_Status.name_room).filter(models.User_Status.name_room != 'Hell').all()
+    
+    
+    users_info =[]
+    for user in users:
+        user_info = {
+            "id": user.id,
+            "user_name": user.user_name,
+            "avatar": user.avatar,
+            "created_at": user.created_at
+        }
+        users_info.append(user_schema.UserOut(**user_info))
 
-    # merge result
+    # Prepare room info
     rooms_info = []
     for room in rooms:
         room_info = {
@@ -87,5 +75,9 @@ def get_rooms(substring: str, db: Session = Depends(get_db)):
             "secret_room": room.secret_room
         }
         rooms_info.append(schema_room.RoomBase(**room_info))
-        
-    return rooms_info
+    
+    # Return the results
+    return {
+        "users": users_info,
+        "rooms": rooms_info
+    }
