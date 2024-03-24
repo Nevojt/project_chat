36f44cef065e352990a2887fd8c56589046cf421
Nevojt <nevojt@gmail.com>
2024-03-24 19:40:02 +0100

    created test room update

--- tests/tesr_rooms/test_rooms.py
+++ tests/tesr_rooms/test_rooms.py
@@ -30,6 +30,12 @@ def test_room():
             "image_room": "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/Content%20Home%20page/Desktop/Danger%20in%20nature.jpg",
             "secret_room": False}
     
+@pytest.fixture
+def test_update_room():
+    return {"name_room": "Test Room Update",
+            "image_room": "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/Content%20Home%20page/Desktop/Winter%20fun.jp",
+            "secret_room": True}
+    
 def test_get_all_rooms(client):
     response = client.get("/rooms")
     assert response.status_code == 200
@@ -55,7 +61,38 @@ async def test_created_rooms(test_user, test_room):
 
     
 
+def test_get_one_name(client, test_room):
+    room = test_room['name_room']
+    response = client.get(f"/rooms/{room}")
+    assert response.status_code == 200
+    assert response.json()['name_room'] == test_room['name_room']
+    
+    
+@pytest.mark.asyncio
+async def test_update_rooms(test_user,test_room, test_update_room):
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+        assert login_res.status_code == 200
+        login_data = login_res.json()
+        token = login_data['access_token']
+        
+        headers = {"Authorization": f"Bearer {token}"}
+        
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        room = test_room['name_room']
+        response = await client.get(f"rooms/{room}")
+        assert response.status_code == 200
+        room_data = response.json()
+        id_room = room_data['id']
 
+        
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.put(f"/rooms/{id_room}",
+                            headers=headers, 
+                            json=test_update_room,
+                            follow_redirects=False)
+        assert response.status_code == 200
+        assert response.json()['name_room'] == test_update_room['name_room']
 
 
 
@@ -79,9 +116,5 @@ async def test_get_rooms_manager(test_user):
         
         
     
-def test_get_one_name(client, test_room):
-    room = test_room['name_room']
-    response = client.get(f"/rooms/{room}")
-    assert response.status_code == 200
-    assert response.json()['name_room'] == test_room['name_room']
+
     
