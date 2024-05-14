e693edfe944662bcd8b0c03c3c01ae57ed5e4257
Nevojt <nevojt@gmail.com>
2024-05-14 20:56:23 +0200

    created documentation

--- app/routers/room/role_in_room.py
+++ app/routers/room/role_in_room.py
@@ -9,14 +9,26 @@ from app.models import models
 
 router = APIRouter(
     prefix="/role-in-room",
-    tags=['Role Im Rooms']
+    tags=['Role In Rooms']
 )
 
-# Get info role current user
+
 @router.get('/')
 async def list_role_in_room(db: AsyncSession = Depends(get_async_session), 
                           current_user: models.User = Depends(oauth2.get_current_user)):
-    
+    """
+    Retrieves the role in room for the authenticated user.
+
+    Parameters:
+    db (AsyncSession): The database session for asynchronous operations.
+    current_user (models.User): The current user making the request.
+
+    Returns:
+    List[models.RoleInRoom]: A list of RoleInRoom objects for the specified user.
+
+    Raises:
+    HTTPException: If the user with the given ID is not found in the database.
+    """
     role_query = select(models.RoleInRoom).where(models.RoleInRoom.user_id == current_user.id)
     result = await db.execute(role_query)
     role_in_room = result.scalars().all()
@@ -31,7 +43,21 @@ async def list_role_in_room(db: AsyncSession = Depends(get_async_session),
 @router.get('/admin/{user_id}')
 async def list_role_in_room(user_id: int, db: AsyncSession = Depends(get_async_session), 
                           current_user: models.User = Depends(oauth2.get_current_user)):
-    """Admin option"""
+    """
+    Admin option to get role in room for a specific user.
+
+    Parameters:
+    user_id (int): The ID of the user whose role in room needs to be fetched.
+    db (AsyncSession): The database session for asynchronous operations.
+    current_user (models.User): The current user making the request.
+
+    Returns:
+    List[models.RoleInRoom]: A list of RoleInRoom objects for the specified user.
+
+    Raises:
+    HTTPException: If the user with the given ID is not found in the database.
+    HTTPException: If the current user is not an admin.
+    """
     
     role_query = select(models.RoleInRoom).where(models.RoleInRoom.user_id == user_id)
     result = await db.execute(role_query)
@@ -44,3 +70,4 @@ async def list_role_in_room(user_id: int, db: AsyncSession = Depends(get_async_s
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
         
     return role_in_room
+
