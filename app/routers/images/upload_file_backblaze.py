92cb9a577fb6155ed2aa642b46761ac469dbe99a
Nevojt <nevojt@gmail.com>
2024-05-23 22:24:58 +0200

    new bucket

--- app/routers/images/upload_file_backblaze.py
+++ app/routers/images/upload_file_backblaze.py
@@ -16,9 +16,25 @@ router = APIRouter(
     prefix="/upload-to-backblaze",
     tags=['Upload file'],
 )
-
+bucket = ["chatall", "usravatar"]
 @router.post("/chat")
-async def upload_to_backblaze(file: UploadFile = File(..., limit="25MB"), bucket_name: str = Query(default="chatall")):
+async def upload_to_backblaze(file: UploadFile = File(..., limit="25MB"), bucket_name: str = Query("chatall", inclusion=bucket)):
+    """
+    This function is responsible for uploading a file to a specified Backblaze B2 bucket.
+
+    Parameters:
+    file (UploadFile): The file to be uploaded. The file size limit is set to 25MB.
+    bucket_name (str): The name of the Backblaze B2 bucket to upload the file to. The default value is "chatall".
+                        Download avatars: bucket -> "usravatar"
+                        The bucket_name must be one of the values in the 'bucket' list.
+
+    Returns:
+    JSONResponse: A JSON response containing the public URL of the uploaded file.
+                  If an error occurs during the upload process, a HTTPException is raised with a 500 status code.
+
+    Raises:
+    HTTPException: If an error occurs during the upload process.
+    """
     try:
         # Створення тимчасового файлу
         with NamedTemporaryFile(delete=False) as temp_file:
