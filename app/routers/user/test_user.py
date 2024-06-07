e61fd9df8b99826be6bbd2648c4e3224e272e614
Nevojt <nevojt@gmail.com>
2024-06-07 21:55:39 +0200

    test 2

--- app/routers/user/test_user.py
+++ app/routers/user/test_user.py
@@ -40,25 +40,25 @@ from app.auth import oauth2
 #                     assert mocked_token.called
 
 
-@pytest.mark.asyncio
-async def test_login_simple_db_access():
-    user_data = {
-        "username": "test@example.com",
-        "password": "testpassword"
-    }
+# @pytest.mark.asyncio
+# async def test_login_simple_db_access():
+#     user_data = {
+#         "username": "test@example.com",
+#         "password": "testpassword"
+#     }
 
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        with patch('app.database.async_db.get_async_session', new_callable=AsyncMock) as mocked_session:
-            mocked_db = mocked_session.return_value.__aenter__.return_value
-            # Setup mock to return a user object, ensuring it's an awaited call in the actual application
-            mocked_db.execute.return_value.scalar_one_or_none.return_value = AsyncMock(
-                email="user@example.com", 
-                password="hashed_password", 
-                blocked=False
-            )
+#     async with AsyncClient(app=app, base_url="http://test") as ac:
+#         with patch('app.database.async_db.get_async_session', new_callable=AsyncMock) as mocked_session:
+#             mocked_db = mocked_session.return_value.__aenter__.return_value
+#             # Setup mock to return a user object, ensuring it's an awaited call in the actual application
+#             mocked_db.execute.return_value.scalar_one_or_none.return_value = AsyncMock(
+#                 email="user@example.com", 
+#                 password="hashed_password", 
+#                 blocked=False
+#             )
 
-            response = await ac.post("/login", data=user_data)
+#             response = await ac.post("/login", data=user_data)
 
-    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
-    if response.status_code != 200:
-        print("Response data:", response.json())
+#     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+#     if response.status_code != 200:
+#         print("Response data:", response.json())
