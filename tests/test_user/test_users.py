a29e752aab7f5ae73b262bc059bf263bcf84b705
Nevojt <nevojt@gmail.com>
2024-06-08 11:53:39 +0200

    test test

--- tests/test_user/test_users.py
+++ tests/test_user/test_users.py
@@ -69,17 +69,17 @@ def test_login_user(test_user):
     assert login_res.token_type == "bearer"
     
     
-# @pytest.mark.parametrize("email, password, status_code", [
-#     (None, 'password123', 422),
-#     ('sanjeev@gmail.com', None, 422)
-# ])
+@pytest.mark.parametrize("email, password, status_code", [
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+])
     
-# def test_incorrect_login(email, password, status_code):
-#     res = client.post(
-#         "/login", 
-#         data={"username": email, "password": password})
+def test_incorrect_login(email, password, status_code):
+    res = client.post(
+        "/login", 
+        data={"username": email, "password": password})
 
-#     assert res.status_code == status_code
+    assert res.status_code == status_code
 
 # def test_incorrect_error():
 #     res = client.post(
@@ -88,144 +88,145 @@ def test_login_user(test_user):
 #     assert res.status_code == 401
 
 # # @pytest.mark.asyncio
-# # async def test_verify_user(test_user):
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         # Authenticate the user
-# #         login_res = await client.post("/login", 
-# #                                       data={"username": test_user['email'],
-# #                                             "password": test_user['password']})
-# #         assert login_res.status_code == 200
-# #         login_data = login_res.json()
-# #         token = login_data['access_token']
-        
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         # Get user's information to find the user ID
-# #         user_info_res = await client.get("/users/", 
-# #                                          headers={"Authorization": f"Bearer {token}"})
-# #         assert user_info_res.status_code == 200
-# #         users = user_info_res.json()
-        
-# #         # Find the specific user
-# #         user = next((u for u in users if u['email'] == test_user['email']), None)
-# #         assert user is not None
-# #         token_verify = user['token_verify']
+@pytest.mark.asyncio
+async def test_verify_user(test_user):
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Authenticate the user
+        login_res = await client.post("/login", 
+                                      data={"username": test_user['email'],
+                                            "password": test_user['password']})
+        assert login_res.status_code == 200
+        login_data = login_res.json()
+        token = login_data['access_token']
 
-# #     headers = {
-# #         "Authorization": f"Bearer {token}"
-# #     }
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Get user's information to find the user ID
+        user_info_res = await client.get("/users/", 
+                                         headers={"Authorization": f"Bearer {token}"})
+        assert user_info_res.status_code == 200
+        users = user_info_res.json()
 
-# #     # # The 'put' request must also be inside the 'async with' block
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         response = await client.get(f"/success_registration",
-# #                                     headers=headers,
-# #                                     params={'token': token_verify},
-# #                                     follow_redirects=False
-# #                                     )
+        # Find the specific user
+        user = next((u for u in users if u['email'] == test_user['email']), None)
+        assert user is not None
+        token_verify = user['token_verify']
 
-# #     # Step 3: Assert the response
-# #     assert response.status_code == 200 
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    # # The 'put' request must also be inside the 'async with' block
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get(f"/success_registration",
+                                    headers=headers,
+                                    params={'token': token_verify},
+                                    follow_redirects=False
+                                    )
+
+    # Step 3: Assert the response
+    assert response.status_code == 200
     
-# # @pytest.mark.asyncio
-# # async def test_update_user(test_user, test_user_update):
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         # Authenticate the user
-# #         login_res = await client.post("/login", 
-# #                                       data={"username": test_user['email'],
-# #                                             "password": test_user['password']})
-# #         assert login_res.status_code == 200
-# #         login_data = login_res.json()
-# #         token = login_data['access_token']
+@pytest.mark.asyncio
+async def test_update_user(test_user, test_user_update):
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Authenticate the user
+        login_res = await client.post("/login", 
+                                      data={"username": test_user['email'],
+                                            "password": test_user['password']})
+        assert login_res.status_code == 200
+        login_data = login_res.json()
+        token = login_data['access_token']
         
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         # Get user's information to find the user ID
-# #         user_info_res = await client.get("/users/", 
-# #                                          headers={"Authorization": f"Bearer {token}"})
-# #         assert user_info_res.status_code == 200
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Get user's information to find the user ID
+        user_info_res = await client.get("/users/", 
+                                         headers={"Authorization": f"Bearer {token}"})
+        assert user_info_res.status_code == 200
 
 
-# #     # Step 2: Update the user
-# #     new_data = {
-# #         "user_name": test_user_update["user_name"],
-# #         "avatar": test_user_update["avatar"]
-# #     }
-# #     headers = {
-# #         "Authorization": f"Bearer {token}"
-# #     }
+    # Step 2: Update the user
+    new_data = {
+        "user_name": test_user_update["user_name"],
+        "avatar": test_user_update["avatar"]
+    }
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
 
-# #     # # The 'put' request must also be inside the 'async with' block
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         response = await client.put(f"/users/",
-# #                                     headers=headers,
-# #                                     json=new_data,
-# #                                     follow_redirects=False
-# #                                     )
+    # # The 'put' request must also be inside the 'async with' block
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.put(f"/users/",
+                                    headers=headers,
+                                    json=new_data,
+                                    follow_redirects=False
+                                    )
 
-# #     # Step 3: Assert the response
-# #     assert response.status_code == 200
-# #     assert response.json()['user_name'] == test_user_update["user_name"]
+    # Step 3: Assert the response
+    assert response.status_code == 200
+    assert response.json()['user_name'] == test_user_update["user_name"]
 
 
 
     
-# # @pytest.fixture
-# # def test_user_new_password():
-# #     return {"email":"test_user@gmail.com",
-# #             "password":"NewPassword"}
+@pytest.fixture
+def test_user_new_password():
+    return {"email":"test_user@gmail.com",
+            "password":"NewPassword"}
 
-# # @pytest.mark.asyncio
-# # async def test_change_user_password(test_user, test_user_new_password):
-# #     # Authenticate the user to get a token
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         # Authenticate the user
-# #         login_res = await client.post("/login",
-# #                                       data={"username": test_user['email'],
-# #                                             "password": test_user['password']})
-# #         assert login_res.status_code == 200
-# #         login_data = login_res.json()
-# #         token = login_data['access_token']
+@pytest.mark.asyncio
+async def test_change_user_password(test_user, test_user_new_password):
+    # Authenticate the user to get a token
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Authenticate the user
+        login_res = await client.post("/login",
+                                      data={"username": test_user['email'],
+                                            "password": test_user['password']})
+        assert login_res.status_code == 200
+        login_data = login_res.json()
+        token = login_data['access_token']
 
-# #     headers = {
-# #         "Authorization": f"Bearer {token}",
-# #     }
-# #     change_password = {
-# #         "old_password": test_user['password'],
-# #         "new_password": test_user_new_password['password']
-# #     }
+    headers = {
+        "Authorization": f"Bearer {token}",
+    }
+    change_password = {
+        "old_password": test_user['password'],
+        "new_password": test_user_new_password['password']
+    }
 
-# #     # Attempt to change the user's password using the 'put' method with JSON body
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         res_password = await client.put("/manipulation/password",
-# #                                         headers={"Authorization": f"Bearer {token}"},
-# #                                         json=change_password)
+    # Attempt to change the user's password using the 'put' method with JSON body
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        res_password = await client.put("/manipulation/password",
+                                        headers={"Authorization": f"Bearer {token}"},
+                                        json=change_password)
         
         
-# #         assert res_password.status_code == 200
+        assert res_password.status_code == 200
 
 
-# # @pytest.mark.asyncio
-# # async def test_delete_user(test_user_new_password):
-# #     # Authenticate the user to get a token
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         # Authenticate the user
-# #         login_res = await client.post("/login", data={"username": test_user_new_password['email'],
-# #                                                       "password": test_user_new_password['password']})
-# #         assert login_res.status_code == 200
-# #         login_data = login_res.json()
-# #         token = login_data['access_token']
+@pytest.mark.asyncio
+async def test_delete_user(test_user_new_password):
+    # Authenticate the user to get a token
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Authenticate the user
+        login_res = await client.post("/login", data={"username": test_user_new_password['email'],
+                                                      "password": test_user_new_password['password']})
+        assert login_res.status_code == 200
+        login_data = login_res.json()
+        token = login_data['access_token']
     
-# #     headers = {
-# #         "Authorization": f"Bearer {token}",
-# #     }
-# #     data = {
-# #         "password": test_user_new_password['password']
-# #     }
+    headers = {
+        "Authorization": f"Bearer {token}",
+    }
+    data = {
+        "password": test_user_new_password['password']
+    }
 
-# #     # Attempt to delete the user using the 'request' method with JSON body
-# #     async with AsyncClient(app=app, base_url="http://test") as client:
-# #         delete_res = await client.request("DELETE", "/users/", 
-# #                                           headers=headers, json=data)
+    # Attempt to delete the user using the 'request' method with JSON body
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        delete_res = await client.request("DELETE", "/users/", 
+                                          headers=headers, json=data)
 
-# #     assert delete_res.status_code == 204
+    assert delete_res.status_code == 204
 
 
 
