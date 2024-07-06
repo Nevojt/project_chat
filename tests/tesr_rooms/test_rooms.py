41d097e3440a61a7954bcb7809731dfbd5a56f2c
Nevojt <nevojt@gmail.com>
2024-07-06 10:04:27 +0200

    test room conflict

--- tests/tesr_rooms/test_rooms.py
+++ tests/tesr_rooms/test_rooms.py
@@ -1,11 +1,8 @@
-import json
+
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
-from sqlalchemy.orm import Session
-from app.schemas import room as room_schemas
 import asyncio
-from app.database.database import engine
 from httpx import AsyncClient
 
 client = TestClient(app)
@@ -22,7 +19,7 @@ async def async_session():
     from app.database import get_async_session  # Імпорт get_async_session тут
     async for session in get_async_session():
         yield session
-    
+    await session.rollback()
     
 @pytest.fixture
 def test_user():
@@ -33,98 +30,98 @@ def test_user():
 def test_room():
     return {"name_room": "Test Room Created",
             "image_room": "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/Content%20Home%20page/Desktop/Danger%20in%20nature.jpg",
-            "secret_room": False}
-    
-@pytest.fixture
-def test_update_room():
-    return {"name_room": "Test Room Update",
-            "image_room": "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/Content%20Home%20page/Desktop/Winter%20fun.jp",
             "secret_room": True}
     
-# def test_get_all_rooms(client):
-#     response = client.get("/rooms")
-#     assert response.status_code == 200
-    
-@pytest.mark.asyncio
-async def test_created_rooms(test_user, test_room, async_session):
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
-        assert login_res.status_code == 200
-        login_data = login_res.json()
-        token = login_data['access_token']
-        
-        headers = {"Authorization": f"Bearer {token}"}
-        
-        
-        
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.post("/rooms/",
-                            headers=headers, 
-                            json=test_room,
-                            follow_redirects=False)
-        assert response.status_code == 201
+
 
     
-@pytest.mark.asyncio
-async def test_get_one_name(test_room, async_session):
-    room = test_room['name_room']
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.get(f"/rooms/{room}")
-        assert response.status_code == 200
-        assert response.json()['name_room'] == test_room['name_room']
+# @pytest.mark.asyncio
+# async def test_created_rooms(test_user, test_room, async_session):
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+#         assert login_res.status_code == 200
+#         login_data = login_res.json()
+#         token = login_data['access_token']
+        
+#         headers = {"Authorization": f"Bearer {token}"}
+        
+        
+        
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         response = await client.post("/rooms/",
+#                             headers=headers, 
+#                             json=test_room,
+#                             follow_redirects=False)
+#         assert response.status_code == 201
+
     
+# @pytest.mark.asyncio
+# async def test_get_one_name(test_room, async_session):
+#     room = test_room['name_room']
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         response = await client.get(f"/rooms/{room}")
+#         assert response.status_code == 200
+#         assert response.json()['name_room'] == test_room['name_room']
     
-@pytest.mark.asyncio
-async def test_update_rooms(test_user, test_room, test_update_room, async_session):
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
-        assert login_res.status_code == 200
-        login_data = login_res.json()
-        token = login_data['access_token']
-        
-        headers = {"Authorization": f"Bearer {token}"}
-        
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        room = test_room['name_room']
-        response = await client.get(f"rooms/{room}")
-        assert response.status_code == 200
-        room_data = response.json()
-        id_room = room_data['id']
-
-        
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.put(f"/rooms/{id_room}",
-                            headers=headers, 
-                            json=test_update_room,
-                            follow_redirects=False)
-        assert response.status_code == 200
-        assert response.json()['name_room'] == test_update_room['name_room']
 
 
+# @pytest.fixture
+# def test_update_room():
+#     return {"name_room": "Test Room Update",
+#             "image_room": "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/Content%20Home%20page/Desktop/Winter%20fun.jp",
+#             "secret_room": True}
+     
+# @pytest.mark.asyncio
+# async def test_update_rooms(test_user, test_room, test_update_room, async_session):
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+#         assert login_res.status_code == 200
+#         login_data = login_res.json()
+#         token = login_data['access_token']
+        
+#         headers = {"Authorization": f"Bearer {token}"}
+        
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         room = test_room['name_room']
+#         response = await client.get(f"rooms/{room}")
+#         assert response.status_code == 200
+#         room_data = response.json()
+#         id_room = room_data['id']
 
-@pytest.mark.asyncio
-async def test_delete_rooms(test_user, test_update_room, async_session):
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
-        assert login_res.status_code == 200
-        login_data = login_res.json()
-        token = login_data['access_token']
         
-        headers = {"Authorization": f"Bearer {token}"}
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         response = await client.put(f"/rooms/{id_room}",
+#                             headers=headers, 
+#                             json=test_update_room,
+#                             follow_redirects=False)
+#         assert response.status_code == 200
+#         assert response.json()['name_room'] == test_update_room['name_room']
+
+
+
+# @pytest.mark.asyncio
+# async def test_delete_rooms(test_user, test_update_room, async_session):
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         login_res = await client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+#         assert login_res.status_code == 200
+#         login_data = login_res.json()
+#         token = login_data['access_token']
         
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        room = test_update_room['name_room']
-        response = await client.get(f"rooms/{room}")
-        assert response.status_code == 200
-        room_data = response.json()
-        id_room = room_data['id']
+#         headers = {"Authorization": f"Bearer {token}"}
+        
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         room = test_update_room['name_room']
+#         response = await client.get(f"rooms/{room}")
+#         assert response.status_code == 200
+#         room_data = response.json()
+#         id_room = room_data['id']
 
         
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.delete(f"/rooms/{id_room}",
-                            headers=headers,
-                            follow_redirects=False)
-        assert response.status_code == 204
+#     async with AsyncClient(app=app, base_url="http://test") as client:
+#         response = await client.delete(f"/rooms/{id_room}",
+#                             headers=headers,
+#                             follow_redirects=False)
+#         assert response.status_code == 204
     
 
 
