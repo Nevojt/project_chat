d78a6bc3678b1615f39cebedbcb285897752e8cb
Nevojt <nevojt@gmail.com>
2024-07-09 20:56:42 +0200

    start admin function created get room for user is login to company

--- app/routers/room/rooms.py
+++ app/routers/room/rooms.py
@@ -435,3 +435,67 @@ async def block_room(room_id: int,
     
     status_text = "unblocked" if not room.block else "blocked"
     return {"message": f"Room with ID: {room_id} has been {status_text}"}
+
+
+
+
+@router.get("/company/v2/", response_model=List[room_schema.RoomBase])
+async def get_rooms_info_company(current_user: models.User = Depends(oauth2.get_current_user),
+                         db: Session = Depends(get_db)):
+    
+    """
+    Retrieves information about chat rooms, excluding a specific room ('Hell'), along with associated message and user counts.
+
+    Args:
+        db (Session, optional): Database session dependency. Defaults to Depends(get_db).
+
+    Returns:
+        List[schemas.RoomBase]: A list containing information about each room, such as room name, image, count of users, count of messages, and creation date.
+    """
+    company_id = current_user.company_id 
+    # get info rooms and not room "Hell"
+    # rooms = db.query(models.Rooms).filter(models.Rooms.name_room != 'Hell', models.Rooms.secret_room != True).order_by(asc(models.Rooms.id)).all()
+    rooms = db.query(
+        models.Rooms.id,
+        models.Rooms.owner,
+        models.Rooms.name_room,
+        models.Rooms.image_room,
+        models.Rooms.created_at,
+        models.Rooms.secret_room,
+        models.Rooms.block,
+        models.Rooms.delete_at,
+        func.count(models.Socket.id).label('count_messages')
+    ).outerjoin(models.Socket, models.Rooms.name_room == models.Socket.rooms) \
+    .filter(models.Rooms.name_room != 'Hell', models.Rooms.secret_room != True, models.Rooms.company_id == company_id) \
+    .group_by(models.Rooms.id) \
+    .order_by(desc('count_messages')) \
+    .all()
+    # Count messages for room
+    messages_count = db.query(
+        models.Socket.rooms, 
+        func.count(models.Socket.id).label('count')
+    ).group_by(models.Socket.rooms).filter(models.Socket.rooms != 'Hell').all()
+
+    # Count users for room
+    users_count = db.query(
+        models.User_Status.name_room, 
+        func.count(models.User_Status.id).label('count')
+    ).group_by(models.User_Status.name_room).filter(models.User_Status.name_room != 'Hell').all()
+
+    # merge result
+    rooms_info = []
+    for room in rooms:
+        room_info = {
+            "id": room.id,
+            "owner": room.owner,
+            "name_room": room.name_room,
+            "image_room": room.image_room,
+            "count_users": next((uc.count for uc in users_count if uc.name_room == room.name_room), 0),
+            "count_messages": next((mc.count for mc in messages_count if mc.rooms == room.name_room), 0),
+            "created_at": room.created_at,
+            "secret_room": room.secret_room,
+            "block": room.block
+        }
+        rooms_info.append(room_schema.RoomBase(**room_info))
+
+    return rooms_info
