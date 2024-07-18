45d23cfb6468697b4b13196baf807e2881bebced
Nevojt <nevojt@gmail.com>
2024-07-18 20:28:53 +0200

    created delete user and get users for admin company

--- app/admin/user.py
+++ app/admin/user.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 from typing import List
 from fastapi import Form, Response, status, HTTPException, Depends, APIRouter, UploadFile, File
 
@@ -116,3 +117,84 @@ async def created_user_admin(email: str = Form(...),
     await say_hello_system(new_user.id)
     
     return new_user
+
+
+@router.get("/company", response_model=List[user.UserInfoLights])
+async def read_company_users(db: AsyncSession = Depends(get_async_session),
+                             current_user: user.UserOut = Depends(oauth2.get_current_user)):
+    
+    if current_user.role != 'admin':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You are not authorized to read users in this company")
+    
+    query_users = select(user_model.User).where(user_model.User.company_id == current_user.company_id)
+    result = await db.execute(query_users)
+    users = result.scalars().all()
+    
+    
+    return users
+
+
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(
+    user_id: int,
+    db: AsyncSession = Depends(get_async_session), 
+    current_user: int = Depends(oauth2.get_current_user)
+):
+    """
+    Asynchronously deletes a user from the database.
+
+    This endpoint allows a user to delete their own profile by providing their user ID and password. It performs several checks to ensure that the user is allowed to delete the profile, such as verifying the user's identity, checking the existence of the user, and ensuring the user is verified and authorized to perform the deletion.
+
+    Parameters:
+    - id (int): The unique identifier of the user to be deleted.
+    - password (str): The password of the user to authenticate the deletion request.
+    - db (AsyncSession): The database session used to perform database operations.
+    - current_user (int): The user ID obtained from the current user session, used to ensure a user can only delete their own profile.
+
+    Raises:
+    - HTTPException: If the current user is not the user being deleted, if the user does not exist, if the user is not verified, or if the provided password is incorrect.
+
+    Returns:
+    - Response: An empty response with a 204 No Content status, indicating successful deletion.
+    """
+    
+    if current_user.role != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You are not authorized to delete users.")
+    
+    query = select(user_model.User).where(user_model.User.id == user_id)
+    result = await db.execute(query)
+    existing_user = result.scalar_one_or_none()
+
+    # If the user does not exist, raise a 404 error
+    if not existing_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with ID  does not exist.")
+
+        
+    query_room = select(room_model.Rooms).where(room_model.Rooms.owner == user_id)
+    result_room = await db.execute(query_room)
+    rooms_to_update = result_room.scalars().all()
+    
+    for room in rooms_to_update:
+        query_moderators = select(room_model.RoleInRoom).where(room_model.RoleInRoom.room_id == room.id, room_model.RoleInRoom.role == 'moderator')
+        result_moderators = await db.execute(query_moderators)
+        moderator = result_moderators.scalars().first()
+        
+        message = f"Room {room.name_room} is now owned by YOU"
+        if moderator:
+            room.owner = moderator.user_id
+            moderator.role = 'owner'
+            await system_notification_change_owner(moderator.user_id, message)
+        else:
+            room.owner = current_user.id
+
+        
+    await db.commit()
+
+    
+    # delete user
+    await db.delete(existing_user)
+    await db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
